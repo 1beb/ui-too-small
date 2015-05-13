a0de889c1669a9139828d8f9d3112c87dbbac2d2
Brandon Bertelsen <brandon.bertelsen@credoconsulting.ca>
2015-05-13 04:19:43 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+define(function (require, exports, module) {
+    "use strict";
+
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+
+    ExtensionUtils.addEmbeddedStyleSheet("* { font-size: 22px; }");
+});
