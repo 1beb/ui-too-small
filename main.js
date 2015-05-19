dc87771b99d31927c9e288e9c8f5a78f1a386adb
Paul Wellner Bou <paul@wellnerbou.de>
2015-05-19 10:04:20 +0200

    Fix font size, line height and selection background

--- main.js
+++ main.js
@@ -3,5 +3,13 @@ define(function (require, exports, module) {
 
     var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
-    ExtensionUtils.addEmbeddedStyleSheet("* { font-size: 22px !important; line-height: 25px !important; }");
+    ExtensionUtils.addEmbeddedStyleSheet("#sidebar *, #main-toolbar *, #titlebar *, #problems-panel *,"+
+	"#find-in-files-results *, #e4b-main-panel *, #status-bar *,"+
+	"#main-toolbar *, #context-menu-bar *, #codehint-menu-bar *,"+
+	"#quick-view-container *, #function-hint-container *  { font-size: 22px !important;"+
+	" line-height: 25px !important; }"+
+	".sidebar li { min-height: 25px !important;}"+
+	".sidebar-selection, .filetree-selection { min-height: 30px !important; margin-top: 3px;}"+
+	""
+	);
 });
